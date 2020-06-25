0efae863fa30ac73b8217b4d9b0137eb6c9a4e1c
Chris Arakelian <coolacpc@gmail.com>
2020-06-25 09:56:54 +0300

    "ZenHairStaff Todo App V1.0"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import TodoForm from "./components/TodoForm";
+import TodoList from './components/TodoList';
+
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo) {
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return{
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
-      <h2>Chris Arakelian</h2>
+      <Typography style={{ padding: 16 }} variant="h1">
+        ZenHairStaff Todo List
+      </Typography>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo}/>
     </div>
   );
 }
